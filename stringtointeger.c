6e424f5b2761c6c63282ba44cefd55a9b5d0bef6
Guganesan.E <guganesh36@gmail.com>
2016-06-28 14:31:39 +0530

    Create stringtointeger.c

---
+++ stringtointeger.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int p;
+char g[256];
+clrscr();
+printf("Enter the number: ");
+scanf("%s",g);
+p=atoi(g);
+printf("The value %d",p);
+getch();
+}
